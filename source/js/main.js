cbffb8e2a88638e4f2d8e3f576bfe70432117b03
kovtonyuk <s.kovtonyuk@gmail.com>
2018-06-01 16:42:49 +0300

    add files

---
+++ source/js/main.js
@@ -0,0 +1,23 @@
+// Открытие формы
+(function () {
+    var openFormButton = document.querySelector('.arrow-down');
+    var form = document.querySelector('.form');
+
+    if(openFormButton) {
+        openFormButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            SK.form.open();
+        })
+    }
+
+    if (form) {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            if (SK.form.isValid()) {
+                console.log('All good');
+            } else {
+                console.log('Is not valid');
+            }
+        })
+    }
+}());
