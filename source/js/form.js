82d8e9c68bd526e1ebc44cc730d84c5b42e047a2
kovtonyuk <s.kovtonyuk@gmail.com>
2018-06-01 16:27:04 +0300

    add validation and required for fields

--- source/js/form.js
+++ source/js/form.js
@@ -1,3 +1,4 @@
+// Открытие формы
 (function () {
     var me = {};
     var form = document.querySelector('.form-container');
@@ -5,11 +6,17 @@
 
 
     function onClose() {
-        me.close();
+
+        me.close()
         closeButton.removeEventListener('click', onClose);
     }
 
     me.open = function() {
+        // плавность открытия формы
+        document.querySelector('.form-container').animate({
+            opacity: [ 0, 1 ]
+        }, 500);
+
         form.classList.remove('is-hidden');
 
         closeButton = document.querySelector('.form__close-button');
@@ -18,21 +25,49 @@
     };
 
     me.close = function() {
-        form.classList.add('is-hidden');
+        // Плавность закрытия формы
+        var formAnimation = form.animate({
+            opacity: [ 1, 0 ]
+        }, 300);
+
+        formAnimation.addEventListener('finish', function() {
+            form.classList.add('is-hidden');
+        });
     };
 
     document.addEventListener('keydown', function(e) {
-        e.preventDefault();
         var key = e.which || e.keyCode || 0;
-        if (key == 27) {
+        if(key == 27) {
             onClose();
         }
     });
 
-    document.getElementsByClassName('arrow-down').onclick = function() {
-        document.querySelector('.form-container').fadeIn(500);
-        document.querySelector('.form-container').fadeOut(500);
+    me.isValid = function () {
+        if (!me.isAllCompleted(document.querySelectorAll('[data-valid="required"]'))) {
+            console.log('Заполните обязательные поля');
+            return false;
+        } else if (!SK.validation.isEmail(document.querySelector('[data-email]'))) {
+            console.log('Не верно указан email');
+            return false;
+        } else if (!SK.validation.isNumber(document.querySelector('[data-email]'))) {
+            console.log('Не верно указан номер телефона');
+            return false;
+        }
+
+        return true;
     };
 
-    window.form = me;
+    me.isAllCompleted = function (data) {
+        var result = true;
+
+        for(var i=0; i < data.length; i++) {
+            if (!SK.validation.isNotEmpty(data[i].value)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    };
+
+    SK.form = me;
 }());
